ed6989fa8cd20c6a8e892aa44eb350c0284ca7ff
Will W <wjwat@onionslice.org>
2022-03-11 11:26:40 -0600

    Fix names to reference current project

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using BestRestaurant.Models;
+using HairSalon.Models;
 
 namespace HairSalon
 {
@@ -25,7 +25,7 @@ namespace HairSalon
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<BestRestaurantContext>(options => options
+        .AddDbContext<HairSalonContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
