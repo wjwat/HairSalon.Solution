599fefbd0708a4232ec8a554658ea6683c837968
Will W <wjwat@onionslice.org>
2022-03-11 13:04:13 -0600

    Build out routing for Clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -23,5 +23,43 @@ namespace HairSalon.Controllers
       // Display list of current clients
       return View();
     }
+
+    public ActionResult Create()
+    {
+      // If we have no stylists just dump back out to the create Stylist page,
+      // but a more ideal solution would be to add an intermediate page that
+      // says why we're back at the create a stylist page.
+      if (_db.Stylists.Count() == 0)
+      {
+        return RedirectToAction("Create", "Stylists");
+      }
+      else
+      {
+        ViewBag.StylistIds = new SelectList(_db.Stylists, "StylistId", "Name");
+        return View();
+      }
+    }
+
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      return View();
+    }
+
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeletePost(int id)
+    {
+      return View();
+    }
   }
 }
