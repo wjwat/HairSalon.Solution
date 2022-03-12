dba472ac76036e248bf642a7c13cb9060164252b
Will W <wjwat@onionslice.org>
2022-03-12 07:13:04 -0600

    Add form partials for client & stylist

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,9 +4,15 @@ namespace HairSalon.Models
 {
   public class Client
   {
+    public Client()
+    {
+      this.Appointments = new HashSet<Appointment>();
+    }
+
     public int ClientId { get; set; }
     public int StylistId { get; set; }
     public string Name { get; set; }
     public virtual Stylist Stylist { get; set; }
+    public virtual ICollection<Appointment> Appointments { get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,13 +7,23 @@ namespace HairSalon.Models
     public Stylist()
     {
       this.Clients = new HashSet<Client>();
+      this.Appointments = new HashSet<Appointment>();
     }
 
     // Is there a specific need to have this column named 'StylistId' instead of
     // 'Id'?
     public int StylistId { get; set; }
-    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Name
+    {
+      get
+      {
+        return FirstName + " " + LastName;
+      }
+    }
 
     public virtual ICollection<Client> Clients { get; set; }
+    public virtual ICollection<Appointment> Appointments { get; set; }
   }
 }
